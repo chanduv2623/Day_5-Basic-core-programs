1c905f15eb8675e3864cfad8ef6764ebcd475f0c
chandu.v <chandrasekhar.veeramallu@gmail.com>
2023-06-27 12:15:21 +0530

    Even or Odd

--- Day_5-Basic-core-programs/Program.cs
+++ Day_5-Basic-core-programs/Program.cs
@@ -4,18 +4,18 @@
     {
         static void Main(string[] args)
         {
-            int Number1, Number2, Swaping_Numbers; // Inputs variables;
-            Console.WriteLine("\nInput the First Number : ");
-            Number1 = int.Parse(Console.ReadLine());  // parse converting string to integer;
-            Console.WriteLine("\nInput the Second Number : ");
-            Number2 = int.Parse(Console.ReadLine());
-            Swaping_Numbers = Number1;
-            Number1 = Number2;
-            Number2 = Swaping_Numbers;
-            Console.Write("\nAfter Swapping : ");
-            Console.Write("\nFirst Number : " + Number1);
-            Console.Write("\nSecond Number : " + Number2);
-            Console.Read();
+            int Number1, Remainder1;
+            Console.Write("\n\n");
+            Console.WriteLine("Check whether a number is even or odd :\n");
+            Console.WriteLine("---------------------------------------");
+            Console.Write("\n\n");
+            Console.WriteLine("Input an integer : ");
+            Number1 = Convert.ToInt32(Console.ReadLine());
+            Remainder1 = Number1 % 2;
+            if (Remainder1 == 0)
+                Console.WriteLine("{0} is an even integer.\n", Number1);
+            else
+                Console.WriteLine("{0} is an odd integer.\n", Number1);
         }
     }
     
