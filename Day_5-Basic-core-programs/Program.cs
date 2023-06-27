ca25073b359e139f10e2c78739bf6488664213c5
chandu.v <chandrasekhar.veeramallu@gmail.com>
2023-06-27 11:51:51 +0530

    Power of 2

--- Day_5-Basic-core-programs/Program.cs
+++ Day_5-Basic-core-programs/Program.cs
@@ -4,23 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(" Please enter a year : ");
-            int Leapyear = Convert.ToInt32(Console.ReadLine());
+            int Number, power;
+            int result;
+            Console.WriteLine("\n\n Recursion : Calculate power of any number :\n");
+            Console.WriteLine("------------------------------------------------\n");
 
-            if (Leapyear < 1000 || Leapyear > 9999)
-            {
-                Console.WriteLine("Invalid year. Please enter a 4-digit year.");
-            }
-            else if ((Leapyear % 4 == 0 && Leapyear % 100 != 0) || Leapyear % 400 == 0)
-            {
-                Console.WriteLine("{0} is a leap year.", Leapyear);
-            }
+            Console.WriteLine(" Input the base  value : ");
+            Number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" Input the exponent : ");
+            power = Convert.ToInt32(Console.ReadLine());
+
+            result = CalOfPower(Number, power);//called the function CalOfPower
+
+            Console.WriteLine(" The value of {0} to the power of {1} is : {2} \n\n", Number, power, result);
+        }
+
+        public static int CalOfPower(int x, int y)
+        {
+            if (y == 0)
+                return 1;
             else
-            {
-                Console.WriteLine("{0} is not a leap year.", Leapyear);
-            }
-
-            Console.ReadLine();
+                return x * CalOfPower(x, y - 1);
         }
     }
     
