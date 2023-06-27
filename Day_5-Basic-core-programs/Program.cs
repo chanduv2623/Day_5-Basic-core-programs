090cc938691d54c92f8da4e6db5885e3d01e69bd
chandu.v <chandrasekhar.veeramallu@gmail.com>
2023-06-27 12:06:45 +0530

    Compute Quotient & Remainder

--- Day_5-Basic-core-programs/Program.cs
+++ Day_5-Basic-core-programs/Program.cs
@@ -4,18 +4,27 @@
     {
         static void Main(string[] args)
         {
-            int n, i;
-            Console.WriteLine("Please Enter the Number:");
-            n = int.Parse(Console.ReadLine());
-            Console.WriteLine("Factors:");
-            for (i = 1; i <= n; i++)
-            {
-                if (n % i == 0)
-                {
-                    Console.WriteLine(i);
-                }
-            }
-            Console.ReadLine();
+
+            //declare and initialize character variable
+
+            int a = 12, b = 2;
+            int q, r;
+
+            //find the quotient
+
+            q = a / b;
+
+            //find the remainder
+
+            r = a % b;
+
+            // display result
+
+            Console.WriteLine("When " + a + " is divided by " + b + ", quotient is " + q + " and remainder is " + r + ".");
+
+            // wait for user to press any key
+
+            Console.ReadKey();
         }
     }
     
